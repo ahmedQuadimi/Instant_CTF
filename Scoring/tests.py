c3d1126dffcfbb00ff2e02a542658de60bab9a19
Mouad Bensafir <mouadensafir@gmail.com>
2026-04-04 12:22:32 +0100

    Add Score Decay Function

--- Scoring/tests.py
+++ Scoring/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+from Scoring.utils import calculate_points
+
+
+class CalculatePointsTests(SimpleTestCase):
+    def test_returns_max_points_for_first_blood(self):
+        self.assertEqual(calculate_points(500, 100, 0.05, 0), 500)
+
+    def test_applies_exponential_decay(self):
+        self.assertEqual(calculate_points(500, 100, 0.05, 10), 303)
+
+    def test_never_returns_less_than_min_points(self):
+        self.assertEqual(calculate_points(500, 100, 0.05, 100), 100)

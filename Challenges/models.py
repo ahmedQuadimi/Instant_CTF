d9932310cf495d11ed07c353e8460bd9b5565784
mnl <manalzaidi2005@gmail.com>
2026-04-04 13:02:37 +0100

    Add Challenge status and connection info fields

--- Challenges/models.py
+++ Challenges/models.py
@@ -11,6 +11,12 @@ class Challenge(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=50)
     description = models.TextField()
+    connection_info = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=10,
+        choices=[("VISIBLE", "Visible"), ("HIDDEN", "Hidden")],
+        default="HIDDEN",
+    )
     state = models.CharField(
         max_length=20,
         choices=[("HIDDEN", "Hidden"), ("VISIBLE", "Visible")],
